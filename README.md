34e56fe4fecd1c6f49659cf3705ca930ba9c6752
unknown <fernando_ho93@hotmail.com>
2025-12-26 19:22:22 -0300

    chore: initial cypress login tests

---
+++ README.md
@@ -0,0 +1,29 @@
+﻿# Cypress Iniciante
+
+Projeto de testes E2E com Cypress para a loja de demonstração em https://automationpratice.com.br.
+
+## Pré-requisitos
+- Node.js 18+
+- npm ou yarn
+
+## Instalação
+`ash
+npm install
+`
+
+## Como rodar
+- Rodar em modo headless:
+`ash
+npx cypress run --spec cypress/e2e/login.cy.js --config video=false
+`
+- Abrir o Test Runner interativo:
+`ash
+npx cypress open
+`
+
+## Estrutura
+- cypress.config.js: baseUrl configurado para o ambiente público.
+- cypress/e2e/login.cy.js: cenários de login (sucesso, validações, lembrar-me/link de cadastro) usando seletores da página.
+
+## Notas
+- Artefatos de execução (vídeos, screenshots) não estão versionados; adicione um .gitignore se for subir ao GitHub.
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,39 @@
+﻿describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('/login');
+  });
+
+  it('login com sucesso 3', () => {
+    cy.get('#top_header').as('cabecalho').contains('Login');
+    cy.get('@cabecalho').find('.fa-user');
+
+    cy.get('.header-logo');
+
+    cy.get('#user').type('usuario@teste.com');
+    cy.get('#password').type('123456');
+    cy.get('#btnLogin').click();
+
+    cy.url().should('include', '/my-account');
+  });
+
+  it('exibe erro para email invalido', () => {
+    cy.get('#user').type('invalido');
+    cy.get('#password').type('123456');
+    cy.get('#btnLogin').click();
+
+    cy.get('.invalid_input').should('contain', 'E-mail inválido.');
+  });
+
+  it('exibe erro para senha curta', () => {
+    cy.get('#user').type('usuario@teste.com');
+    cy.get('#password').type('123');
+    cy.get('#btnLogin').click();
+
+    cy.get('.invalid_input').should('contain', 'Senha inválida.');
+  });
+
+  it('permite marcar lembrar e acessar cadastro', () => {
+    cy.get('#materialUnchecked').check().should('be.checked');
+    cy.get('#createAccount').should('have.attr', 'href', '/register');
+  });
+});
